8e96f9b6fcb3287fdef3039a06e8dd26d82bd633
Joab Almeida <joabalmeida.dev@gmail.com>
2025-01-29 18:37:13 -0300

    Pass by value vs reference

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 'use strict';
-
+/*
 //create array to the push new bookings
 const bookings = [];
 
@@ -28,3 +28,35 @@ createBooking('LH123', 2, 800);
 createBooking('LH123', 5);
 createBooking('LH123', 2, 500);
 createBooking('LH123', undefined, 200);
+*/
+
+const flight = 'LH234';
+const joab = {
+  name: 'Joab Almeida',
+  passport: 123456789,
+};
+const checkIn = function (flightNum, passenger) {
+  flightNum = 'LH999';
+  passenger.name = 'Mr. ' + passenger.name;
+
+  if (passenger.passport === 123456789) {
+    alert('Check in');
+  } else {
+    alert('Wrong passport!');
+  }
+};
+
+/*
+checkIn(flight, joab);
+console.log(flight);
+console.log(joab);
+//the primitive value of "flight" didn't change, but the object joab did, because objects change if referenced.
+//flightNum is just a copy of flight, not really used, but passenger references joab, so it changes both, because object goe sinto function heap
+*/
+const newPassport = function (person) {
+  person.passport = Math.trunc(Math.random() * 10000000000);
+};
+
+newPassport(joab);
+checkIn(flight, joab);
+//when we change the passport, the previous if condition fails. And it changes because the newPassport references the object joab and changes value, doesn't create a new variable inside itself and then changes it
